1864c2a0b63a1d848a5b74acbb2ebf5ace1792d6
Angel D. Munoz <scythes.lol@gmail.com>
2019-08-19 22:07:16 -0600

    feature(items):  address issues #1 #2

--- Trackmat.Lib/Runners/ItemRunner.cs
+++ Trackmat.Lib/Runners/ItemRunner.cs
@@ -3,21 +3,35 @@ using LiteDB;
 using Trackmat.Lib.Enums;
 using Trackmat.Lib.Interfaces;
 using Trackmat.Lib.Models;
+using Trackmat.Lib.Services;
 
 namespace Trackmat.Lib.Runners
 {
-  public class ItemRunner : IRunnable
+  public class ItemRunner : IRunnable<TrackItem>
   {
-    public int Create<T>(T target)
+    protected TrackItemService _items { get; private set; } = new TrackItemService();
+
+
+    public int Create(TrackItem target)
     {
-      var item = target as TrackItem;
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine(item);
-      Console.ResetColor();
+      try
+      {
+        var created = _items.Create(target);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Saved [{created.Id} - {created.Item}] Successfully \n{created}");
+        Console.ResetColor();
+      }
+      catch (Exception e)
+      {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Failed to Create {target.Item}. {e.Message}");
+        Console.ResetColor();
+        return (int)ExitCodes.FailedToCreateItem;
+      }
       return (int)ExitCodes.Success;
     }
 
-    public int Delete<T>(T target)
+    public int Delete(TrackItem target)
     {
       throw new NotImplementedException();
     }
@@ -27,7 +41,12 @@ namespace Trackmat.Lib.Runners
       throw new NotImplementedException();
     }
 
-    public int Update<K, T>(K identifier, T target)
+    public int Update(string name, TrackItem target)
+    {
+      throw new NotImplementedException();
+    }
+
+    public int Update(ObjectId id, TrackItem target)
     {
       throw new NotImplementedException();
     }
