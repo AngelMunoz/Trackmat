f47b6c08b27e75d52735695f20788e927982d56e
Angel D. Munoz <scythes.lol@gmail.com>
2019-09-26 01:24:54 -0600

    feature(tests): use a temporary database to run tests feature(tests): add PeriodServiceTest

---
+++ Trackmat.UnitTests/Services/PeriodServiceTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using LiteDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trackmat.Lib.Models;
+
+namespace Trackmat.Lib.Services.Tests
+{
+  [TestClass()]
+  public class PeriodServiceTest
+  {
+    private string homepath;
+    private LiteDatabase _db;
+    private TrackItemService _items;
+    private PeriodService _periods;
+
+    [TestInitialize()]
+    public void StartUp()
+    {
+      homepath = Path.Combine(Path.GetTempPath(), "trackmat");
+      Directory.CreateDirectory(homepath);
+      _db = new LiteDatabase(Path.Combine(homepath, "trackmat.db"));
+      _items = new TrackItemService(_db);
+      _periods = new PeriodService(_db);
+    }
+
+    [TestCleanup()]
+    public void CleanUp()
+    {
+      _items.Dispose();
+      _periods.Dispose();
+      if (Directory.Exists(homepath))
+      {
+        Directory.Delete(homepath, true);
+      }
+    }
+
+    [TestMethod]
+    public void CreateTest()
+    {
+      var period = _periods.Create(new Period
+      {
+        Name = "Sprint 5",
+        EzName = "s5",
+        StartDate = DateTime.Now,
+        EndDate = DateTime.Now.AddDays(1),
+        Items = new List<TrackItem>()
+      });
+
+      Assert.IsNotNull(period.Id);
+      Assert.AreEqual(DateTime.Now.ToString("yyyy-MM-dd"), period.StartDate.ToString("yyyy-MM-dd"));
+      Assert.AreEqual(DateTime.Now.AddDays(1).ToString("yyyy-MM-dd"), period.EndDate.ToString("yyyy-MM-dd"));
+      Assert.AreEqual(0, period.Items.Count());
+    }
+
+    [TestMethod]
+    public void FindOneTest()
+    {
+      var expected = _periods.Create(new Period
+      {
+        Name = "FindOne",
+        EzName = "F1",
+        StartDate = DateTime.Now,
+        EndDate = DateTime.Now.AddDays(1),
+        Items = new List<TrackItem>()
+      });
+
+      var actual = _periods.FindOne("F1");
+
+      Assert.AreEqual(expected.EzName, actual.EzName);
+      Assert.AreEqual(expected.Name, actual.Name);
+      Assert.AreEqual(
+        expected.StartDate.ToString("yyyy-MM-dd"),
+        actual.StartDate.ToString("yyyy-MM-dd")
+      );
+      Assert.AreEqual(
+        expected.EndDate.ToString("yyyy-MM-dd"),
+        actual.EndDate.ToString("yyyy-MM-dd")
+      );
+      Assert.AreEqual(expected.Items.Count(), actual.Items.Count());
+    }
+
+    [TestMethod]
+    public void ExistsTest()
+    {
+      _periods.Create(new Period
+      {
+        Name = "Exists",
+        EzName = "E1",
+        StartDate = DateTime.Now,
+        EndDate = DateTime.Now.AddDays(1),
+        Items = new List<TrackItem>()
+      });
+
+      var upperCase = _periods.Exists("E1");
+      var lowerCase = _periods.Exists("e1");
+      Assert.IsTrue(upperCase);
+      Assert.IsTrue(lowerCase);
+    }
+
+  }
+}
---
+++ Trackmat.UnitTests/Services/TrackItemServiceTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using LiteDB;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trackmat.Lib.Models;
+
+namespace Trackmat.Lib.Services.Tests
+{
+  [TestClass()]
+  public class TrackItemServiceTest
+  {
+    private string homepath;
+    private LiteDatabase _db;
+    private TrackItemService _items;
+
+    [TestInitialize()]
+    public void StartUp()
+    {
+      homepath = Path.Combine(Path.GetTempPath(), "trackmat");
+      Directory.CreateDirectory(homepath);
+      _db = new LiteDatabase(Path.Combine(homepath, "trackmat.db"));
+      _items = new TrackItemService(_db);
+    }
+
+    [TestCleanup()]
+    public void CleanUp()
+    {
+      _items.Dispose();
+      if (Directory.Exists(homepath))
+      {
+        Directory.Delete(homepath, true);
+      }
+    }
+
+    [TestMethod]
+    public void CreateTest()
+    {
+      var item = _items.Create(new TrackItem
+      {
+        Item = "SMPL-001",
+        Time = 0.5f,
+        Date = DateTime.Now
+      });
+
+      Assert.IsNotNull(item.Id);
+      Assert.AreEqual(DateTime.Now.ToString("yyyy-MM-dd"), item.Date.ToString("yyyy-MM-dd"));
+      Assert.AreEqual("SMPL-001", item.Item);
+      Assert.AreEqual(0.5f, item.Time);
+      Assert.IsNull(item.Url);
+    }
+
+    [TestMethod]
+    public void FindOneTest()
+    {
+      var item = _items.Create(new TrackItem
+      {
+        Item = "SMPL-001",
+        Time = 0.5f,
+        Date = DateTime.Now,
+        Url = "https://localhost/browse/SMPL-001"
+      });
+
+      var found = _items.FindOne(item.Id);
+
+      Assert.IsNotNull(found.Id);
+      Assert.AreEqual(item.Date.ToString("yyyy-MM-dd"), found.Date.ToString("yyyy-MM-dd"));
+      Assert.AreEqual(item.Item, found.Item);
+      Assert.AreEqual(item.Time, found.Time);
+      Assert.AreEqual(item.Url, found.Url);
+    }
+
+    [TestMethod]
+    public void FindTest()
+    {
+      var result = _items.Find("", new PaginationValues { Page = 1, Limit = 10 });
+      Assert.IsTrue(result.List.Count() == 0);
+      Assert.AreEqual(0, result.Count);
+
+      _items.Create(new TrackItem
+      {
+        Item = "IM-UNIQ",
+        Time = 0.5f,
+        Date = DateTime.Now,
+        Url = "https://localhost/browse/SMPL-001"
+      });
+      var uniq = _items.Find("IM-UNIQ", new PaginationValues { Page = 1, Limit = 10 });
+      Assert.IsTrue(uniq.List.Count() == 1);
+      Assert.AreEqual(1, uniq.Count);
+    }
+  }
+}
