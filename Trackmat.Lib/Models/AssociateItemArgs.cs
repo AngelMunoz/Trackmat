151eb3c5a81f98c0677724127e88de8fa96da1e9
Angel D. Munoz <scythes.lol@gmail.com>
2019-09-25 20:51:10 -0600

    feature(period): add association of items

---
+++ Trackmat.Lib/Models/AssociateItemArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trackmat.Lib.Models
+{
+  public class AssociateItemArgs
+  {
+    public string EzName { get; set; }
+    public IEnumerable<string> Items { get; set; }
+  }
+}
