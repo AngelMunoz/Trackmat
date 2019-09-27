1040cc8a099c0b680a73f73e299492d0963c98e7
Angel D. Munoz <scythes.lol@gmail.com>
2019-09-27 16:11:28 -0600

    feature(cli.extensions): Add a extension method

---
+++ Trackmat.Cli/Extensions/DatabasePath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Trackmat.Cli.Extensions
+{
+  public static class DatabasePath
+  {
+    public static string GetDatabasePath(this string dbname)
+    {
+      return Path.Combine(Environment.GetEnvironmentVariable("TRACKMAT_HOME", EnvironmentVariableTarget.User) ?? Environment.GetEnvironmentVariable("TRACKMAT_HOME"), dbname);
+    }
+  }
+}
